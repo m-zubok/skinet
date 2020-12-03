01974601ff4de80137e9d218f66113d62f7733c3
M.Zubok <maxym.zubok@gmail.com>
2020-12-03 19:55:32 +0200

    Initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using Core.Entities;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly StoreContext _context;
+
+		public ProductsController(StoreContext context) {
+			_context = context;
+		}
+
+		[HttpGet]
+		public async Task<ActionResult<Product[]>> GetProducts() {
+			var products = await _context.Products.ToArrayAsync();
+			return Ok(products);
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Product>> GetProduct(int id) {
+			var product = await _context.Products.FindAsync(id);
+			return Ok(product);
+		}
+	}
+}
