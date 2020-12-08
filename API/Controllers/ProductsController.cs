30da95b2357ae2efc2c6f0d6726afb4a02e1195c
M.Zubok <maxym.zubok@gmail.com>
2020-12-08 19:53:04 +0200

    End of section 3

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using API.Data;
 using Core.Entities;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -15,22 +15,46 @@ namespace API.Controllers
 	[ApiController]
 	public class ProductsController : ControllerBase
 	{
-		private readonly StoreContext _context;
+		private readonly IProductRepository _repository;
 
-		public ProductsController(StoreContext context) {
-			_context = context;
+		public ProductsController(IProductRepository repository) {
+			_repository = repository;
 		}
 
 		[HttpGet]
 		public async Task<ActionResult<Product[]>> GetProducts() {
-			var products = await _context.Products.ToArrayAsync();
+			var products = await _repository.GetProductsAsync();
 			return Ok(products);
 		}
 
 		[HttpGet("{id}")]
 		public async Task<ActionResult<Product>> GetProduct(int id) {
-			var product = await _context.Products.FindAsync(id);
+			var product = await _repository.GetProductByIdAsync(id);
 			return Ok(product);
 		}
+
+		[HttpGet("brands")]
+		public async Task<ActionResult<IReadOnlyList<ProductBrand>>> GetBrands() {
+			var brands = await _repository.GetProductBrandsAsync();
+			return Ok(brands);
+		} 
+
+		//[HttpGet("brands/{id}")]
+		//public async Task<ActionResult<ProductBrand>> GetType(int id) {
+		//	var brand = await _repository.GetProductBrandByIdAsync(id);
+		//	return Ok(brand);
+		//} 
+
+		[HttpGet("types")]
+		public async Task<ActionResult<IReadOnlyList<ProductType>>> GetTypes() {
+			var types = await _repository.GetProductTypesAsync();
+			return Ok(types);
+		} 
+
+		//[HttpGet("types/{id}")]
+		//public async Task<ActionResult<ProductType>> GetBrand(int id) {
+		//	var type = await _repository.GetProductTypeByIdAsync(id);
+		//	return Ok(type);
+		//} 
 	}
 }
---
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -0,0 +1,51 @@
+﻿using Core.Entities;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+	public class StoreContextSeed
+	{
+		public static async Task SeedAsync(StoreContext context, ILoggerFactory loggerFactory) {
+			try {
+				if (!context.ProductBrands.Any()) {
+					var brandsData = File.ReadAllText("../Infrastructure/Data/SeedData/brands.json");
+					var brands = JsonSerializer.Deserialize<List<ProductBrand>>(brandsData);
+					foreach (var item in brands) {
+						context.ProductBrands.Add(item);
+					}
+
+					await context.SaveChangesAsync();
+				}
+
+				if (!context.ProductTypes.Any()) {
+					var productTypes = File.ReadAllText("../Infrastructure/Data/SeedData/types.json");
+					var types = JsonSerializer.Deserialize<List<ProductType>>(productTypes);
+					foreach (var item in types) {
+						context.ProductTypes.Add(item);
+					}
+
+					await context.SaveChangesAsync();
+				}
+
+				if (!context.Products.Any()) {
+					var products = File.ReadAllText("../Infrastructure/Data/SeedData/products.json");
+					var types = JsonSerializer.Deserialize<List<Product>>(products);
+					foreach (var item in types) {
+						context.Products.Add(item);
+					}
+
+					await context.SaveChangesAsync();
+				}
+			} catch (Exception ex) {
+				loggerFactory.CreateLogger<StoreContextSeed>().LogError(ex, "Error seed data");
+			}
+		}
+	}
+}
